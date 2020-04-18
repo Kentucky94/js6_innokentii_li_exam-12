fea7bf4b0f9b180587978cb3d3bcb73dfb60f669
Kentucky94 <lis5043@gmail.com>
2020-04-18 12:52:09 +0600

    Fixtures added

--- backend/app/pictures.js
+++ backend/app/pictures.js
@@ -22,7 +22,51 @@ const upload = multer({storage});
 
 router.post('/', [auth, upload.single('image')], async (req, res) => {
   try{
+    const pictureData = {
+      author: req.user._id,
+      name: req.body.name,
+      image: req.file.filename,
+    };
 
+    const picture = new Picture(pictureData);
+
+    await picture.save();
+
+    return res.send(picture);
+  }catch(error){
+    return res.status(400).send(error);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try{
+    const pictures = await Picture.find().populate({path: 'author', select: ['displayName']});
+
+    return res.send(pictures);
+  }catch(error){
+    return res.status(400).send(error);
+  }
+});
+
+router.get('/:userId', async (req, res) => {
+  try{
+    const pictures = await Picture.find({author: req.params.userId}).populate({path: 'author', select: ['displayName']});
+
+    return res.send(pictures);
+  }catch(error){
+    return res.status(400).send(error);
+  }
+});
+
+router.delete('/:pictureId', auth, async (req, res) => {
+  try{
+    const picture = await Picture.findOne({_id: req.params.pictureId});
+
+    if(String(picture.author._id) !== String(req.user._id)) return res.status(400).send({error: 'Invalid user'});
+
+    await Picture.deleteOne({_id: req.params.pictureId});
+
+    res.send({message: 'Picture has been deleted'});
   }catch(error){
     return res.status(400).send(error);
   }
--- backend/fixtures.js
+++ backend/fixtures.js
@@ -3,6 +3,7 @@ const {nanoid} = require('nanoid');
 
 const config = require('./config');
 const User = require('./models/User');
+const Picture = require('./models/Picture');
 
 const run = async () => {
   await mongoose.connect(config.database, config.databaseOptions);
@@ -13,7 +14,7 @@ const run = async () => {
     await mongoose.connection.db.dropCollection(coll.name);
   }
 
-  const [user1, user2, admin] = await User.create({
+  const [user1, user2, user3] = await User.create({
     username: 'user1',
     password: 'password1',
     token: nanoid(),
@@ -24,11 +25,36 @@ const run = async () => {
     token: nanoid(),
     displayName: 'William Birkin',
   }, {
-    username: 'admin',
-    password: 'admin123',
-    role: 'admin',
+    username: 'user3',
+    password: 'password3',
     token: nanoid(),
-    displayName: 'Admin Adminov',
+    displayName: 'Jill Valentine',
+  });
+
+  await Picture.create({
+    author: user1,
+    name: 'Falling Anton',
+    image: '/fixtures/image1.jpg'
+  }, {
+    author: user1,
+    name: 'Magma',
+    image: '/fixtures/image2.jpg'
+  }, {
+    author: user2,
+    name: 'In Utero',
+    image: '/fixtures/image3.jpg'
+  }, {
+    author: user2,
+    name: 'Unicorn',
+    image: '/fixtures/image4.jpg'
+  }, {
+    author: user3,
+    name: 'Morning Glory',
+    image: '/fixtures/image5.jpg'
+  }, {
+    author: user3,
+    name: 'Sparkle, Sparkle',
+    image: '/fixtures/image6.jpg'
   });
 
   mongoose.connection.close();
